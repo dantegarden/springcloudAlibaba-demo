8da9c75d761ab83f9732718ea2af3e2a2ec5d279
dantegarden <761043617@qq.com>
2019-07-10 00:08:51 +0800

    新增gateway、config模块

---
+++ nacos-discovery-config-client/src/main/java/com/example/config/client/NacosDiscoveryConfigClientApplication.java
@@ -0,0 +1,15 @@
+package com.example.config.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class NacosDiscoveryConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosDiscoveryConfigClientApplication.class, args);
+    }
+
+}
--- nacos-discovery-config-client/src/test/java/com/example/config/client/NacosDiscoveryConfigClientApplicationTests.java
+++ nacos-discovery-config-client/src/test/java/com/example/config/client/NacosDiscoveryConfigClientApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.gateway.server;
+package com.example.config.client;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class NacosDiscoveryGatewayServerApplicationTests {
+public class NacosDiscoveryConfigClientApplicationTests {
 
     @Test
     public void contextLoads() {
